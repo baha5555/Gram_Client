5eeb6d85a82ba65b968db5ee7650e835d278f107
bahromkhuja <bahromhuca@gmail.com>
2022-12-07 12:18:09 +0500

    some changes

--- app/src/main/java/com/example/gramclient/Constants.kt
+++ app/src/main/java/com/example/gramclient/Constants.kt
@@ -2,6 +2,6 @@ package com.example.gramclient
 
 object Constants {
     const val LOCAL_BASE_URL="http://10.250.1.114:8080/"
-    const val BASE_URL="http://10.250.1.247:8000/"
+    const val BASE_URL="http://testapi.client.gram.tj/"
     const val PREFIX="992"
 }
