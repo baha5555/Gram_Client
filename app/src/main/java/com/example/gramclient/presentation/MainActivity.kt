800f96b6ef903cdfd4a39b7f32c3138481cece02
Ahroro K <akhrorovk5758@gmail.com>
2023-02-03 16:56:06 +0500

    fix background in DriverInSiteScreen

--- app/src/main/java/com/example/gramclient/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gramclient/presentation/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.gramclient.presentation
 
 import android.content.*
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -41,6 +43,7 @@ class MainActivity : ComponentActivity() {
     private val REQ_USER_CONSENT = 200
     var smsBroadcastReceiver: SmsBroadcastReceiver? = null
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
