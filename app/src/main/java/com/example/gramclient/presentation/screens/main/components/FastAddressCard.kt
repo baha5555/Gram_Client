8fee810ac7450412da0e191e0447f63087731506
P1rnazarov <otabek200101@gmail.com>
2023-02-03 22:27:02 +0500

    fix fast address

--- app/src/main/java/com/example/gramclient/presentation/screens/main/components/FastAddressCard.kt
+++ app/src/main/java/com/example/gramclient/presentation/screens/main/components/FastAddressCard.kt
@@ -3,6 +3,7 @@ package com.example.gramclient.presentation.screens.main.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -24,20 +26,22 @@ import com.example.gramclient.R
 fun FastAddressCard(
     title: String
 ) {
-    Column(
-        modifier = Modifier
-            .width(115.dp)
-//            .height(115.dp)
-            .clip(RoundedCornerShape(20.dp))
-            .clickable { }
-            .background(color = Color(0xFFCFF5FF), shape = RoundedCornerShape(20.dp))
-            .padding(top = 15.dp, start = 15.dp, end = 15.dp)
-    ) {
-        Text(text = title, color = Color.Black, fontSize = 14.sp, fontWeight = FontWeight.Bold)
-        Text(text = "15 мин", color = Color.Black, fontSize = 12.sp)
-        Image(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_car),
-            contentDescription = "car_eco",
-        )
+
+    Box(modifier = Modifier.width(115.dp)){
+        Image(painter = painterResource(id = R.drawable.img_fast_address), contentDescription = null)
+        Image(painter = painterResource(id = R.drawable.back_fast_address), contentDescription = null)
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(20.dp))
+                .clickable { }
+                .padding(top = 15.dp, start = 15.dp, end = 15.dp)
+        ) {
+            Text(text = title, color = Color.Black, fontSize = 14.sp, fontWeight = FontWeight.Bold)
+            Text(text = "15 мин", color = Color.Black, fontSize = 12.sp)
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_car),
+                contentDescription = "car_eco",
+            )
+        }
     }
 }
