339ae8923c187c06369baf66abeedc934f2fb020
otabek <otabek200101@gmail.com>
2023-02-21 19:43:10 +0500

    fix

--- app/src/main/java/com/example/gramclient/domain/mainScreen/Address.kt
+++ app/src/main/java/com/example/gramclient/domain/mainScreen/Address.kt
@@ -7,15 +7,4 @@ data class Address(
     var address_lng: String="",
     var city: String="",
     var idIncrement: Int = 0
-//    val district: String,
-//    val model: String,
-//    val region: String,
-//    val street: String,
-//    val synonym_street: Any,
-//    val synonyms: Any,
-//    val village: String
-)
-data class Address2(
-    var address: String="",
-    var id: Int=0
 )
