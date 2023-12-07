6a4c2f5c922c8cfeecbf157415f8f4cb49491359
baha5555 <bahriddinhaker@gmail.com>
2023-12-07 16:52:11 +0500

    fix bag

--- app/src/main/java/com/gram/client/presentation/screens/main/SearchAddressScreen.kt
+++ app/src/main/java/com/gram/client/presentation/screens/main/SearchAddressScreen.kt
@@ -1,6 +1,8 @@
 package com.gram.client.presentation.screens.main
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -11,12 +13,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -27,9 +34,7 @@ import com.gram.client.presentation.components.*
 import com.gram.client.presentation.components.voyager.MapPointScreen
 import com.gram.client.presentation.components.voyager.SearchAddresses
 import com.gram.client.presentation.screens.drawer.myaddresses_screen.MyAddressViewModel
-import com.gram.client.presentation.screens.main.components.AddressSearchBottomSheet
-import com.gram.client.presentation.screens.main.components.FloatingButton
-import com.gram.client.presentation.screens.main.components.FromAddressField
+import com.gram.client.presentation.screens.main.components.*
 import com.gram.client.presentation.screens.map.CustomMainMap
 import com.gram.client.presentation.screens.map.currentRoute
 import com.gram.client.presentation.screens.map.mLocationOverlay
@@ -47,7 +52,7 @@ class SearchAddressScreen : Screen {
         val drawerState = rememberDrawerState(DrawerValue.Closed)
         val mainViewModel: MainViewModel = hiltViewModel()
         val isSearchState = remember { mutableStateOf(false) }
-        val sheetPeekHeight = remember { mutableStateOf(280) }
+        val sheetPeekHeight = remember { mutableStateOf(350) }
 
 
         val bottomSheetState = rememberBottomSheetScaffoldState(
@@ -143,17 +148,85 @@ class SearchAddressScreen : Screen {
                                 }
                             },
                             drawerGesturesEnabled = false,
-                            sheetBackgroundColor = MaterialTheme.colors.background,
+                            sheetBackgroundColor = Color(0xffEEEEEE),
                             scaffoldState = bottomSheetState,
                             sheetShape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
                             sheetGesturesEnabled = true,
+                            backgroundColor = Color(0xFF702D2D),
                             sheetContent = {
-                                AddressSearchBottomSheet(
-                                    isSearchState = isSearchState,
-                                    mainViewModel = mainViewModel,
-                                    focusRequester = focusRequester,
-                                    toAddress = toAddress
-                                )
+                                val searchText = remember { mutableStateOf("") }
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .focusRequester(focusRequester)
+                                        .fillMaxHeight(fraction = 0.98f)
+                                        .background(Color(0xffEEEEEE)),
+                                ) {
+                                    Column(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(197.dp)
+                                            .padding(2.dp)
+                                            .background(
+                                                color = Color(0xFFFFFFFF),
+                                                shape = RoundedCornerShape(size = 30.dp)
+                                            ), horizontalAlignment = Alignment.CenterHorizontally
+                                    ) {
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxWidth(0.95f)
+                                                .padding(9.dp)
+                                                .background(
+                                                    color = Color(0xFFFFFFFF),
+                                                    shape = RoundedCornerShape(size = 30.dp)
+                                                )
+                                        ) {
+                                        if (!isSearchState.value) {
+                                            if (searchText.value != "") searchText.value = ""
+                                            ToAddressField(
+                                                toAddress = toAddress
+                                            )
+                                            Spacer(modifier = Modifier.height(15.dp))
+                                            FastAddresses(mainViewModel)
+                                        }
+                                        }
+                                    }
+                                    Spacer(modifier = Modifier.height(8.dp))
+
+                                    Column(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .background(
+                                                color = Color(0xFFFFFFFF),
+                                                shape = RoundedCornerShape(size = 30.dp)
+                                            )
+                                    ) {
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(20.dp)
+                                                .background(
+                                                    color = Color(0xFFFFFFFF),
+                                                    shape = RoundedCornerShape(size = 30.dp)
+                                                )
+                                        ) {
+                                            Text(
+                                                text = "Добро пожаловать в Gram",
+                                                style = TextStyle(
+                                                    fontSize = 24.sp,
+                                                    fontWeight = FontWeight(600),
+                                                    color = Color(0xFF343434),
+                                                )
+                                            )
+                                            Spacer(modifier = Modifier.height(10.dp))
+                                            StoriesList()
+                                        }
+
+
+                                    }
+//            Spacer(modifier = Modifier.height(15.dp))
+//            Services()
+                                }
                             },
                             sheetPeekHeight = sheetPeekHeight.value.dp-40.dp,
                         ) {
